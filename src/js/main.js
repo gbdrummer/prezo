cdb47e6dd1b256cd17a743696713928ad37d511b
Graham Butler <graham@grahambutler.com>
2019-03-11 17:42:50 -0700

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+class PrezoController extends NGNX.VIEW.Registry {
+  constructor (cfg) {
+    super(Object.assign({}, {
+      selector: '#prezo',
+      namespace: 'prezo.',
+
+      references: {
+        cycle: '> author-cycle',
+        slides: '> author-cycle > *'
+      }
+    }, cfg))
+  }
+
+  next () {
+    this.ref.cycle.element.next()
+  }
+
+  previous () {
+    this.ref.cycle.element.previous()
+  }
+
+  goto (index) {
+    this.ref.cycle.element.show(index)
+  }
+}
+
+window.Prezo = new PrezoController()
+
+window.Prezo.on({
+  initialized: function () {
+    let { cycle, slides } = this.ref
+
+    slides.each((slide, index) => slide.dataset.index = index)
+
+    cycle.on({
+      beforechange: evt => {
+        evt.preventDefault()
+        let { currentSelection, nextSelection, next } = evt.detail
+
+        this.emit('beforechange', {
+          currentSlide: currentSelection,
+          nextSlide: nextSelection
+        })
+
+        cycle.element.setAttribute('transition', '')
+        currentSelection.element.setAttribute('fade-out', '')
+        setTimeout(next, 500)
+      },
+
+      change: evt => {
+        let { previousSelection, currentSelection } = evt.detail
+
+        this.emit('change', {
+          previousSlide: previousSelection,
+          currentSlide: currentSelection
+        })
+
+        cycle.element.removeAttribute('transition')
+        previousSelection.element.removeAttribute('fade-out')
+        currentSelection.element.setAttribute('fade-in', '')
+        setTimeout(() => currentSelection.element.removeAttribute('fade-in'), 1000)
+      }
+    })
+  }.bind(window.Prezo)
+})
